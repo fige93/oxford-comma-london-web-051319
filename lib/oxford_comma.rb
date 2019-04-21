49e7439c587538521396000906e4a22eda56b49e
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-21 11:23:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  length = array.length
+  if length == 1
+    array.join(", ")
+  elsif length == 2
+    array.join(" and ")
+  else
+    i = 0
+    string = ""
+    while i < length-1
+      string << array[i]
+      string = string + ", "
+      i += 1
+    end
+    string = "#{string}and #{array[i]}"
+  end
 end
