30e51ecc073ab712e6769afc6cd90fa337a94151
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-21 11:20:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  length = array.length
+  if length == 1
+    array.join(", ")
+  else
+    i = 0
+    string = ""
+    while i < length
+      string << array[0]
+      string = string + ", "
+      i += 1
+    end
+    string = "#{string} and #{}"
+  end
 end
